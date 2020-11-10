115eafb4de98ad59478c37c58a1e1473eb86bb98
dilanHewawitharana <dshewawitharana@gmail.com>
2020-11-10 23:02:55 +0900

    rest api basic samples

---
+++ 02_api_test.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {
+    1:{"task": "Write Hello World Program", "summary": "Write the code using python."},
+    2:{"task": "Task 2", "summary": "Writing task 2"},
+    3:{"task": "Task 3", "summary": "this is task 3."}
+}
+
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument("task", type=str, help="Task is required.", required=True)
+task_post_args.add_argument("summary", type=str, help="Summary is required.", required=True)
+
+task_put_args = reqparse.RequestParser()
+task_put_args.add_argument("task", type=str)
+task_put_args.add_argument("summary", type=str)
+
+class ToDoList(Resource):
+    def get(self):
+        return todos
+
+class ToDo(Resource):
+    def get(self, todo_id):
+        return todos[todo_id]
+
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+        if todo_id in todos:
+            abort(409, message="Task ID is already taken")
+        todos[todo_id] = {"task": args["task"], "summary": args["summary"]}
+        return todos[todo_id]
+
+    def put(self, todo_id):
+        args = task_put_args.parse_args()
+        if todo_id not in todos:
+            abort(404, message="Task doesn't exist, cannot update. ")
+        if args['task']:
+            todos[todo_id]['task'] = args['task']
+        if args['summary']:
+            todos[todo_id]['summary'] = args['summary']
+        return todos[todo_id]
+
+    def delete(self, todo_id):
+        del todos[todo_id]
+        return todos
+
+api.add_resource(ToDo, '/todos/<int:todo_id>')
+api.add_resource(ToDoList, '/todos')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
